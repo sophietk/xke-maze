cf1ef2c6d4987e7037a9d6a5b7338da3446e3b4e
sophietk <sophie.tk@gmail.com>
2014-01-22 23:09:30 +0100

    Use Chai for testing

--- test/run.js
+++ test/run.js
@@ -1,14 +1,16 @@
 var maze = require('../maze.js'),
-    assert = require('assert');
+    should = require('chai').should();
 
 describe('maze', function () {
+
     describe('#build()', function () {
+
         it('should build a maze with appropriate size', function () {
             var lines = maze.build(3, 3, 0);
-            assert.equal(3, lines.length);
+            lines.should.have.length(3);
             for (var i in lines) {
                 var line = lines[i];
-                assert(3, line.length);
+                line.should.have.length(3);
             }
         })
     })
@@ -16,14 +18,14 @@ describe('maze', function () {
     describe('#solve()', function () {
         it('should solve a 3x3 maze', function () {
             var lines = [
-                [0, 0, 1],
-                [0, 1, 1],
-                [1, 1, 0]
-            ],
+                    [0, 0, 1],
+                    [0, 1, 1],
+                    [1, 1, 0]
+                ],
                 start = [0, 2],
                 finish = [2, 0];
             var path = maze.solve(lines, start, finish);
-            assert.deepEqual([[2, 0], [2, 1], [1, 1], [1, 0], [0, 0]], path);
+            path.should.be.deep.equal([[2, 0], [2, 1], [1, 1], [1, 0], [0, 0]]);
         })
     })
 });
